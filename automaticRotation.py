207111400021217ab65cc9f3dd8a7dd9d72f2ac5
tassadar <dom@tassadar.net>
2021-11-03 00:54:20 +1100

    WIP - applet updated
    Applet controls mode of service. Notifications can be silenced via config. Code re factored and simplified. Both manual mode and full auto service works perfectly.

--- automaticRotation.py
+++ automaticRotation.py
@@ -19,28 +19,10 @@ from gi.repository import Notify, GdkPixbuf                                     
 from string import digits                                                       # String libary used for rege functions and string manipulation
 from time import sleep                                                          # Required to add easy pause into while loop that samples from acceleromoeter
 ###############################################################################################################################
-
-###############################################################################################################################
-global applicationMode
-APPINDICATOR_ID = "screenrotator"
-orientation = "normal"                                                                               # The Default startip state is assumed to be in laptop configuration
-NotificationIconPath = "/usr/share/ScreenRotationIndicator/notifications.png"                        # require to configure the applications notification icon                                                                           # Pen/Stylus/Digitiser Slave ID
-#MonitorDeviceName = "ELAN0732:00"                                                                    # This is the screens name as per the output of xrandr -q this will be used to try alter the screen state
-IndicatorIconPath = "/usr/share/ScreenRotationIndicator/icon.svg"                                    # Indicator icon location path, This will be programaticly set later
-Notify.init("Screen Rotiation")                                                                      # Initialise the notification object with the applications title, This will appear in bold on the notification toatsy
-applicationMode = "auto"
-##################### Set the tranformation matrix variables ##################################################################
-stringInvertedTransform = "-1 0 1 0 -1 1 0 0 1"
-stringNormalTransform   = "1 0 0 0 1 0 0 0 1"
-stringLeftTransform     = "0 -1 1 1 0 0 0 0 1"
-stringRightTransform    = "0 1 0 -1 0 1 0 0 1"
-
-################################################################################################################################
-notifications = Notify.Notification.new("Screen Rotation Enchanced", "Application Started")          # Create the Notification Object and set it with the start up values ready for display
-image = GdkPixbuf.Pixbuf.new_from_file(NotificationIconPath)                                         # Intialise the image object to be used bu the notification system using GdkPixbuf to create the proper image type
-notifications.set_icon_from_pixbuf(image)                                                            # Set the icon Image for the notifications popup   
-notifications.set_image_from_pixbuf(image)                                                           # Set the image to be used by the notification popup
-notifications.show()                                                                                 # Display the first notification stating that the application has started
+def NotificationToasty (title,message):
+    if ReadConfigFile("notifications") == "enabled":
+        notifications.update(title, message)
+        notifications.show()
 
 def ReadConfigFile(attribute):
     filename = os.path.join(os.path.dirname(__file__), 'autoRotation.config') 
@@ -49,10 +31,8 @@ def ReadConfigFile(attribute):
         for line in file_content:
             attributeFile = re.sub(r'[^a-zA-Z0-9--]', '', re.sub('\:.*$',"",line) )
             if attribute == attributeFile:
-                #data = re.sub(r'[^a-zA-Z0-9--]', '', re.sub('^[^:]*',"", line) )
-                data = re.sub('^[^:]*',"", line)
-                data = data.split(':')
-    return data[1]
+                data = re.sub('^[^:]*',"", line).split(':')
+    return re.sub('\n','', data[1]) #Return value minus new line characters 
 
 def QueryDeviceAddressing():
     global TouchScreenName         # system firendly name as displayed in xinput
@@ -78,7 +58,6 @@ def QueryDeviceAddressing():
         TouchPadDeviceID = re.sub("[^0-9]", "", result.join(result.split('=')[1:]).split(" ",1 )[0])         # Strip out the device ID from the returned String
         TouchPadSlaveID = result.join(result.split('=')[1:]).split("(")[1].rsplit(")")[0]                    # String the Slave ID from the original returned string 
     # Grab the TouchScreen device Numbers
-    #cmdpipe = subprocess.Popen("xinput --list | grep 'ELAN0732:00' | grep -v 'keyboard'  ", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     cmdpipe = subprocess.Popen("xinput --list | grep '" + TouchScreenName + "' | grep -v 'keyboard'  ", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     result = cmdpipe.stdout.readline()
     if result and (not result.isspace()):                                                                    # Execute the above command and return the raw touch screen ID as the PEN/Stylus will have a similar ID the grep -v will remove the any line with PEN in the string
@@ -88,13 +67,11 @@ def QueryDeviceAddressing():
     cmdpipe = subprocess.Popen("xinput --list | grep 'pointer' | grep 'Stylus' | grep 'Pen'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     result = cmdpipe.stdout.readline()  
     if result and (not result.isspace()): 
-        
         DigitiserDeviceID = re.sub("[^0-9]", "", result.join(result.split('=')[1:]).split(" ",1 )[0])        # Strip out the device ID number is regex looking for a numeric charcters after the an =
         DigitiserSlaveID = result.join(result.split('=')[1:]).split("(")[1].rsplit(")")[0]                   # Strip out the slave ID looking for numeric characters between brackets
     else:
         DigitiserDeviceID = ""       
         DigitiserSlaveID = ""
-
 
 def RotateScreen(TargetCordinateMatrix, direction):
     QueryDeviceAddressing()
@@ -105,15 +82,15 @@ def RotateScreen(TargetCordinateMatrix, direction):
     if direction != result:
         #print (TargetCordinateMatrix + "direction target" + direction)
         call(["xrandr", "-o", direction])                      
-        if DigitiserDeviceID and (not DigitiserDeviceID.isspace()):                                                               # Execute screen rotation to the desired orientation
-            command = "xinput set-prop " + DigitiserDeviceID + " 'Coordinate Transformation Matrix' " + TargetCordinateMatrix  #The method above inverts the Pen/Digitiser calibration
+        if DigitiserDeviceID and (not DigitiserDeviceID.isspace()):                                                            # Execute screen rotation to the desired orientation
+            command = "xinput set-prop " + DigitiserDeviceID + " 'Coordinate Transformation Matrix' " + TargetCordinateMatrix  # The method above inverts the Pen/Digitiser calibration
             os.system(command)
-        if TouchScreenDeviceID and (not TouchScreenDeviceID.isspace()):                                                                                                  # Execute the Pen/Digitiser inversion command
+        if TouchScreenDeviceID and (not TouchScreenDeviceID.isspace()):                                                        # Execute the Pen/Digitiser inversion command
             command = "xinput set-prop " + TouchScreenDeviceID + " 'Coordinate Transformation Matrix' " + TargetCordinateMatrix
             #print ("this will be the command - " +command ) # DEBUG
             os.system(command) 
-        notifications.update("Screen Auto Rotation", "Switching rotating " + direction)
-        notifications.show()
+        NotificationToasty ("Screen Auto Rotation", "Switching rotating " + direction)
+
 def DebugPrintDeviceIDS():
     ######################## DEBUG ###########################
     print ("Keyboard ID       - "     + KeyboardDeviceID    )
@@ -125,9 +102,28 @@ def DebugPrintDeviceIDS():
     print ("Touch Screen - "          + TouchScreenDeviceID ) 
     print ("touch Screen Slave ID - " + TouchPadSlaveID     ) 
     ######################## DEBUG ########################### 
-QueryDeviceAddressing()
-#DebugPrintDeviceIDS()
 
+
+###############################################################################################################################
+global applicationMode
+APPINDICATOR_ID = "screenrotator"
+orientation = "normal"                                        # The Default startip state is assumed to be in laptop configuration
+NotificationIconPath = ReadConfigFile("NotificationIconPath")        
+Notify.init(ReadConfigFile("Notify"))                         # Initialise the notification object with the applications title, This will appear in bold on the notification toatsy
+applicationMode = "auto"
+##################### Set the tranformation matrix variables ##################################################################
+stringInvertedTransform = "-1 0 1 0 -1 1 0 0 1"
+stringNormalTransform   = "1 0 0 0 1 0 0 0 1"
+stringLeftTransform     = "0 -1 1 1 0 0 0 0 1"
+stringRightTransform    = "0 1 0 -1 0 1 0 0 1"
+
+################################################################################################################################
+notifications = Notify.Notification.new("Screen Rotation", "Auto Rotation Service Started")          # Create the Notification Object and set it with the start up values ready for display
+image = GdkPixbuf.Pixbuf.new_from_file(NotificationIconPath)                                         # Intialise the image object to be used bu the notification system using GdkPixbuf to create the proper image type
+notifications.set_icon_from_pixbuf(image)                                                            # Set the icon Image for the notifications popup   
+notifications.set_image_from_pixbuf(image)                                                           # Set the image to be used by the notification popup
+NotificationToasty ("Screen Auto Rotation", "Auto rotation Service starting ")                                                                              # Display the first notification stating that the application has started
+QueryDeviceAddressing()                                                                              # Retreive device IDs to be used later 
 
 while applicationMode != "quit":
     acceleromoeterReading = ""
@@ -135,13 +131,9 @@ while applicationMode != "quit":
     cmdpipe = subprocess.Popen("'monitor-sensor'  ", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     for stdout_line in iter(cmdpipe.stdout.readline,""):
         mode = re.sub(r'[^a-zA-Z0-9--]', '', ReadConfigFile("mode") )
-        #print(mode) #debug
         if mode == "auto" or mode =="Auto":
             if stdout_line and (not stdout_line.isspace()): 
-                acceleromoeterReading = re.sub('^[^:]*',"", stdout_line) 
-                acceleromoeterReading = re.sub('[:]','', acceleromoeterReading)
-                acceleromoeterReading = re.sub(' ','', acceleromoeterReading)
-                acceleromoeterReading = re.sub(r'[^a-zA-Z0-9--]', '', acceleromoeterReading)
+                acceleromoeterReading = re.sub(r'[^a-zA-Z0-9--]', '', re.sub(' ','', re.sub('[:]','', re.sub('^[^:]*',"", stdout_line))))
             if acceleromoeterReading == "right-up":
                 RotateScreen (stringRightTransform, "right") 
             elif acceleromoeterReading == "left-up":
